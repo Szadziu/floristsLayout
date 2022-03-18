b9246aa4c6d5f5d983d08a2dabe3f4286fcc2161
Szadziu <mcj.mikolajczak@gmail.com>
2022-03-18 09:21:35 +0100

    added arrow in footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import arrow from '../../assets/icons/down-arrow.png';
 
 const Footer = () => {
   return (
-    <footer className=" grow-1 text-5xl uppercase text-white">footer</footer>
+    <footer className="flex justify-center items-center grow-1 text-5xl uppercase text-white">
+      <img src={arrow} alt="arrow-down" className="w-14" />
+    </footer>
   );
 };
 
